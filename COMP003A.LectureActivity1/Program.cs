9827a5bed14d136f41aa35ed0091911b9153986f
eduardoalcala1774 <eduardoalcala1774@LEM231-20>
2023-01-18 18:56:13 -0800

    Add project files.

---
+++ COMP003A.LectureActivity1/Program.cs
@@ -0,0 +1,19 @@
+﻿// this is a single-line comment
+/* this is a multi-line comment
+    Author: Eduardo Alcala
+    Course: COMP-003A
+    Purpose:Lecture Activity for this week
+ */
+
+namespace COMP003A.LectureActivity1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); //add comments here too
+            Console.WriteLine("this is a string");
+            Console.WriteLine("Eduardo Alcala"); 
+        }
+    }
+}
